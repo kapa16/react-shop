7b508e9524557fe0967eda7e7fdbdcb28b4a202a
Pavel <kapa16@inbox.ru>
2019-02-04 16:26:20 +0500

    component Error boundary

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const ErrorBoundary = (props) => {
+export default class ErrorBoundary extends Component {
 
-  return props.children;
-};
+  state = {
+    hasError: false
+  };
 
-export default ErrorBoundary;
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    })
+  }
+
+  render() {
+
+    if (this.state.hasError) {
+      return <h1>Error in application. We sent bots to fix it.</h1>
+    }
+
+    return this.props.children;
+  }
+}
