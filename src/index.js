531b6f1521678656c52fc7dfb926b7867ab7bbd8
kapa <kapa16@inbox.ru>
2019-02-05 00:21:02 +0500

    delete unused files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducer from './redux/reducer';
+import reducer from './redux/reducers';
 
 import App from './components/App/App';
 
